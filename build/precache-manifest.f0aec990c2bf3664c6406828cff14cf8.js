5f6e7710216be2d13cb9959920ee831970230078
alexandre-okita-ifood <alexandre.okita@ifood.com.br>
2020-07-30 20:14:24 -0300

    iniitial commit

---
+++ build/precache-manifest.f0aec990c2bf3664c6406828cff14cf8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadc44f330f468527833f516979ec76b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d36d2f92748c8654f4a",
+    "url": "/static/css/2.3c1f93d8.chunk.css"
+  },
+  {
+    "revision": "57ee145e650879cff987",
+    "url": "/static/css/main.434da5c3.chunk.css"
+  },
+  {
+    "revision": "9d36d2f92748c8654f4a",
+    "url": "/static/js/2.37d3bab2.chunk.js"
+  },
+  {
+    "revision": "57ee145e650879cff987",
+    "url": "/static/js/main.8964c33b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "891fcd0a0e2029f1e1c252403c67b5b1",
+    "url": "/static/media/AdamTeste.891fcd0a.mp4"
+  },
+  {
+    "revision": "fbc363b3a559cb581026223c71c022cf",
+    "url": "/static/media/VideoMaskedAndNonMaskedPeople.fbc363b3.mp4"
+  },
+  {
+    "revision": "508641e14e230e60b71d5792aa113a90",
+    "url": "/static/media/map.508641e1.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
